6e5d5faeee4fa786dc7a33e42e4fd459dbb923fa
Teroof <lifshesofer@gmail.com>
2024-10-30 18:09:30 +0200

    added Options model

---
+++ SortWords/Models/Options.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortWords.Models
+{
+    internal class Options
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public bool Ascending { get; set; }
+    }
+}
