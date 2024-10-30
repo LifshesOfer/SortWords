014a5bc55c63fc637a424f5d2cce8d8a19f47983
Teroof <lifshesofer@gmail.com>
2024-10-30 18:09:11 +0200

    added FileProcessor class for handling file operations

---
+++ SortWords/FileProcessor.cs
@@ -0,0 +1,33 @@
+﻿namespace SortWords
+{
+    internal class FileProcessor
+    {
+        /// <summary>
+        /// Validates a file path by checking if it is non-empty and accessible with the specified mode and access.
+        /// </summary>
+        /// <param name="filePath">The path of the file to validate. Can be <c>null</c> or empty.</param>
+        /// <param name="fileMode">The <see cref="FileMode"/> to specify how the file should be opened (e.g., Open, Create).</param>
+        /// <param name="fileAccess">The <see cref="FileAccess"/> to specify the level of access (e.g., Read, Write).</param>
+        /// <returns>
+        /// <c>true</c> if the file path is valid and the file can be opened with the given mode and access; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool ValidateFilePath(string? filePath, FileMode fileMode, FileAccess fileAccess)
+        {
+            if(string.IsNullOrEmpty(filePath)) 
+            {
+                Console.WriteLine("File path empty");
+                return false; 
+            }
+            try
+            {
+                using var fs = File.Open(filePath, fileMode, fileAccess);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot open file '{0}' - {1}", filePath, ex.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+}
