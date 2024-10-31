7359a9c19bd46589735b0cd84dc3423469e3ba30
Teroof <lifshesofer@gmail.com>
2024-10-31 13:57:48 +0200

    extracted program flow from ConsoleApp to Configuration.Run (Configuration extension method)

---
+++ SortWords/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using SortWords.Core.Models;
+
+namespace SortWords.Core.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static async Task Run(this Configuration configuration)
+        {
+            var wordProcessor = await FileProcessor.ProcessFile(configuration.InputFile);
+            var uniqueWords = wordProcessor.GetUniqueWords();
+            var sortedWords = uniqueWords.Sort(configuration.Ascending);
+
+            await FileProcessor.WriteFile(configuration.OutputFile, sortedWords);
+            var mostFrequest = wordProcessor.GetMostFrequentWord();
+            Console.WriteLine($"F2 content:\n{sortedWords}");
+            Console.WriteLine($"The most frequent word in the text is '{mostFrequest.Key}', count: {mostFrequest.Value}");
+        }
+    }
+}
