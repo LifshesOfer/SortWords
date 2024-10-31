024d29c6a0dbb690d4df50a974600df43a10e03b
Teroof <lifshesofer@gmail.com>
2024-10-31 15:39:25 +0200

    added MostFrequent model for ease of use with getting the most frequent word or words.

---
+++ SortWords/Models/MostFrequent.cs
@@ -0,0 +1,26 @@
+﻿namespace SortWords.Core.Models
+{
+    public class MostFrequent : IEquatable<MostFrequent>
+    {
+        public string[] Words { get; set; }
+        public int Frequency { get; set; }
+        public MostFrequent()
+        {
+            Words = [];
+            Frequency = 0;
+        }
+
+        public MostFrequent(string[] words, int frequency)
+        {
+            Words = words;
+            Frequency = frequency;
+        }
+
+        public bool Equals(MostFrequent? other)
+        {
+            return (other != null &&
+                Enumerable.SequenceEqual(this.Words, other.Words) &&
+                other.Frequency == this.Frequency);
+        }
+    }
+}
