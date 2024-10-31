07be0a95ccc5ecaa54b21e13430603202cd5cc64
Teroof <lifshesofer@gmail.com>
2024-10-31 16:03:51 +0200

    added additional FileProcessor tests

--- Tester/FileProcessorTests.cs
+++ Tester/FileProcessorTests.cs
@@ -15,6 +15,25 @@ namespace SortWords.Tests
             string outputContent = File.ReadAllText(outputPath).Trim();
             Assert.That(outputContent, Is.EqualTo("apple, banana, grape, orange"));
         }
+
+        [Test]
+        public async Task Test_ProcessFile_NoFile()
+        {
+            string filePath = "test_input.txt";
+            var wordProcessor = await FileProcessor.ProcessFile(filePath);
+            
+            Assert.That(wordProcessor, Is.EqualTo(new WordProcessor()));
+        }
+
+        [Test]
+        public async Task Test_OutputPath_NotValid()
+        {
+            var content = "apple, banana, grape, orange";
+            string outputPath = @"DD:\hello.txt";
+            string resultPath = await FileProcessor.WriteFile(outputPath, content);
+
+            Assert.That(resultPath, Is.EqualTo(string.Empty));
+        }
     }
 
 }
