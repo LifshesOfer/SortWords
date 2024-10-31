35805d2cd34fe96c90fda788d950f6e75e63c07e
Teroof <lifshesofer@gmail.com>
2024-10-31 13:57:10 +0200

    added list<string> extension method for sorting by ascending/descending based on boolean

---
+++ SortWords/Extensions/ListStringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SortWords.Core.Extensions
+{
+    internal static class ListStringExtensions
+    {
+        public static string Sort(this List<string> list, bool ascending)
+        {
+            if (list == null) 
+                return string.Empty;
+            if (ascending)
+            {
+                list.Sort((a,b) => a.CompareTo(b));
+            }
+            else
+            {
+                list.Sort((a, b) => b.CompareTo(a));
+            }
+
+            return string.Join(", ", [.. list]);
+        }
+    }
+}
