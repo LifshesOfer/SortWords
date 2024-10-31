1a32b5b40458f909f27b68cb993a93041cb4de45
Teroof <lifshesofer@gmail.com>
2024-10-31 12:39:15 +0200

    added whitespace regex

---
+++ SortWords/RegularExpressions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SortWords
+{
+    internal static partial class RegularExpressions
+    {
+
+        [GeneratedRegex(@"\s")]
+        public static partial Regex WhitespaceRegex();
+    }
+}
